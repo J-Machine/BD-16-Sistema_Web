8256d400e419d4492abaaee66a456469c0ba67c0
Jenny Ha <jhuancaan@unsa.edu.pe>
2021-12-05 20:24:22 -0500

    Create first mysql query

--- biblioteca.js
+++ biblioteca.js
@@ -4,11 +4,16 @@ var app = express();
 var connection = require('./database');
 
 app.get('/', function(req, res){
-    res.send("Mírame, sin manos!");
+    let libros = "SELECT * FROM libro;"; // Comando SQL
+
+    connection.query(libros, function(err , results){
+        if(err) throw err;
+        // res.send("Mírame, sin manos!");
+        res.send(results);
+    })
 })
 
 app.listen(3000, function(){
-
     console.log("Te escucho en el puerto 3000")
     connection.connect(function(err){
         if(err) throw err;
